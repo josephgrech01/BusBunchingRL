a4824902d78b7b9338eab122531e2e620b2ff534
josephgrech01 <joseph.grech.20@um.edu.mt>
2022-11-15 23:52:48 +0100

    Persons on stops at beginning of simulation

--- randomActions.py
+++ randomActions.py
@@ -12,7 +12,7 @@ for episode in range(1, episodes + 1):
 
     while not done:
         action = random.randint(0,2)
-        state, reward, done, info = env.step(action)
+        state, reward, done, info = env.step(2)#action)
         score += reward
 
     print("Episode: {} Score: {}".format(episode, score))
